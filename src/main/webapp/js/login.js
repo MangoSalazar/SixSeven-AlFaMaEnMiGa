2f6c4a892a4b6c2d0c7735542d9fcf213e099310
fabianMc328 <blackccks020@gmail.com>
2026-05-15 13:44:02 -0700

    realice la logica de la pantalla de los grupos del docente ademas de cargar credenciales simuladas en el js para verificar el correcto flujo del sisistema

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -9,7 +9,7 @@ togglePw.addEventListener('click', () => {
   eyeIcon.textContent = isHidden ? 'visibility_off' : 'visibility';
 });
 
-// Submit con animación
+// Submit con animación y control de accesos 
 function handleLogin() {
   const user = document.getElementById('usuario').value.trim();
   const pass = document.getElementById('password').value;
@@ -27,26 +27,47 @@ function handleLogin() {
     return;
   }
 
-  // Simular carga
+  // Simular carga e interactividad
   btn.disabled = true;
   spinner.style.display = 'block';
   icon.style.display = 'none';
   text.textContent = 'Verificando...';
 
   setTimeout(() => {
-    spinner.style.display = 'none';
-    icon.style.display = '';
-    text.textContent = 'Entrar al sistema';
-    btn.disabled = false;
+    const userUpper = user.toUpperCase();
+    let destino = null;
 
-    // Demo: mostrar error si no es "admin"
-    if (user !== 'admin') {
+    // 1. EVALUACIÓN DE ROLES 
+    // Si inicia con DOC, apunta al perfil de la tabla DOCENTE
+    if (userUpper.startsWith('DOC')) {
+      destino = 'views/Docentes/mis-grupos.html';
+    } 
+    // Si es ADMIN o COORD, apunta a las credenciales de la tabla COORDINADOR
+    else if (userUpper === 'ADMIN' || userUpper === 'COORD') {
+      destino = 'views/coordinador/materias.html';
+    }
+
+    // 2. CONTROL DE FLUJO DE LA INTERFAZ
+    if (!destino) {
+      // Si las credenciales no pertenecen a ningún rol, restaurar botón y mostrar error
+      spinner.style.display = 'none';
+      icon.style.display = '';
+      text.textContent = 'Entrar al sistema';
+      btn.disabled = false;
+
       document.getElementById('alertMsg').textContent = 'Usuario o contraseña incorrectos. Verifica tus datos.';
       alert.classList.add('show');
     } else {
+      spinner.style.display = 'none';
+      icon.style.display = '';
       icon.textContent = 'check_circle';
       btn.style.background = 'var(--mint)';
       text.textContent = '¡Bienvenido!';
+
+      // Retraso de 600ms parea el mensaje de bienvenido
+      setTimeout(() => {
+        window.location.href = destino;
+      }, 600);
     }
   }, 1400);
 }
